31e5676eeeaeed28365af283c274191dd085f8ff
Jhonjandrysdev <jhonjandrysdramirez@unicesar.edu.co>
2023-12-13 22:23:10 -0500

    6

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 const express = require('express')
+
+const multer = require('multer');
+const upload = multer();
+
+
 const app = express()
 const port = process.env.PORT || 3000;
-app.use(express.json());
 
 
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/jonji', (req, res) => {
-  res.json({name:'jonji'})
-  console.log(req.body.name, req.params);
-})
-app.post('/jonji-', (req, res) => {
-  res.json({name:'jonji'})
-  console.log(req.body, req.params);
-})
-app.post('/transaccion', (req, res) => {
+app.post('/transaccion',upload.none(), (req, res) => {
   console.log(req.body, req.params);
   res.json({name:'jonji2'})
 })
