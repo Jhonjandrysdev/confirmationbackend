4c6902f3f1112fa8055fb21dd26d92788aee52e7
Jhonjandrysdev <jhonjandrysdramirez@unicesar.edu.co>
2023-12-13 22:07:12 -0500

    commit

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
-app.use(express.json())
+app.use(express.json());
+
+
+
 app.get('/jonji', (req, res) => {
   res.json({name:'jonji'})
-  console.log(req.body, req.params);
+  console.log(req.body.name, req.params);
 })
 app.post('/jonji-', (req, res) => {
   res.json({name:'jonji'})
@@ -12,7 +15,7 @@ app.post('/jonji-', (req, res) => {
 })
 app.post('/transaccion', (req, res) => {
   console.log(req.body, req.params);
-  res.json({name:'jonji'})
+  res.json({name:'jonji2'})
 })
 
 app.listen(port, () => {
