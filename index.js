c77534e1527240670145a92594bca906e0c58fbb
Jhonjandrysdev <jhonjandrysdramirez@unicesar.edu.co>
2023-12-14 02:03:50 -0500

    12

--- index.js
+++ index.js
@@ -4,17 +4,21 @@ const express = require('express')
 const multer = require('multer');
 const upload = multer();
 
+
+
+
 const app = express()
 const port = process.env.PORT || 3000;
+
 
 var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+const file = __dirname + '/confirmation.txt';
 app.post('/transaccion',upload.none(), (req, res) => {
   const bodyEnString = JSON.stringify(req.body)
   console.log(bodyEnString, req.params);
-  fs.writeFile(bodyEnString, function(err) {
+  fs.writeFile(file,bodyEnString, function(err) {
     if(err) {
         return console.log(err);
     }
@@ -25,7 +29,7 @@ app.post('/transaccion',upload.none(), (req, res) => {
 
 app.get('/obtener-archivo',(req,res)=>{
    
-  res.sendFile(file)
+  res.sendFile(__dirname + '/confirmation.txt')
 })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
